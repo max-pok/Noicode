5b5eae5c1027c9b02c2343fb3ec681b493d89895
max-pok <maxim.p9@gmail.com>
2021-03-19 14:11:05 +0200

    add crypto-js, add http client (axios)

--- client/src/utils/crypto.js
+++ client/src/utils/crypto.js
@@ -3,26 +3,26 @@ require("dotenv").config()
 
 // AES encryption
 export const encrypt = (data) => {
-  let encryptedDate = {}
+  let encryptedData = {}
   const keys = Object.keys(data)
   const values = Object.values(data)
 
   values.forEach((plaintext, index) => {
     const ciphertext = CryptoJS.AES.encrypt(plaintext, process.env.REACT_APP_SECRET_KEY).toString()
-    encryptedDate = { ...encryptedDate, [keys[index]]: ciphertext }
+    encryptedData = { ...encryptedData, [keys[index]]: ciphertext }
   })
-  return encryptedDate
+  return encryptedData
 }
 
 // AES decryption
 export const decrypt = (data) => {
-  let decryptedDate = {}
+  let encryptedData = {}
   const keys = Object.keys(data)
   const values = Object.values(data)
 
   values.forEach((ciphertext, index) => {
     const plaintext = CryptoJS.AES.decrypt(ciphertext, process.env.REACT_APP_SECRET_KEY).toString(CryptoJS.enc.Utf8)
-    decryptedDate = { ...decryptedDate, [keys[index]]: plaintext }
+    encryptedData = { ...encryptedData, [keys[index]]: plaintext }
   })
-  return decryptedDate
+  return encryptedData
 }
