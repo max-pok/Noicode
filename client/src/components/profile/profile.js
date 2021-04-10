f2120752f8165dfa903add36cd3c9b0b1b3a743c
max-pok <maxim.p9@gmail.com>
2021-04-10 16:03:35 +0300

    save userId on registration

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -1,20 +1,33 @@
 import React, { useState, useEffect } from "react"
-import { Avatar, Image } from "antd"
+import { Avatar, Image, Upload, message, Button } from "antd"
+import { UploadOutlined } from "@ant-design/icons"
 import Posts from "./posts/posts"
 import ProfileInfo from "./profile-info/profile-info"
-import "./profile.css"
 import axios from "axios"
+import "./profile.css"
 
 const Profile = (props) => {
   const userInfoUrl = "http://localhost:8080/api/users/" + props.match.params.userId
   const userPostsUrl = "http://localhost:8080/api/posts/" + props.match.params.userId
 
   const [coverImg, setCoverImg] = useState("https://blog.creatopy.com/wp-content/uploads/2018/10/Twitch-Social-Media-Image-Size.jpg")
-  const [profileImg, setProfileImg] = useState("https://max-pok.web.app/assets/images/hero.jpeg")
+  // const [profileImg, setProfileImg] = useState("https://max-pok.web.app/assets/images/hero.jpeg")
+  const [profileImg, setProfileImg] = useState("https://www.blexar.com/avatar.png")
 
   const [userInformation, setUserInformation] = useState({})
   const [userPosts, setUserPosts] = useState([])
   const [userId, setUserId] = useState(props.match.params.userId)
+
+  const handleImageChange = async (info) => {
+    if (info.file.status !== "uploading") {
+      console.log(info.file, info.fileList)
+    }
+    if (info.file.status === "done") {
+      message.success(`${info.file.name} file uploaded successfully`)
+    } else if (info.file.status === "error") {
+      message.error(`${info.file.name} file upload failed.`)
+    }
+  }
 
   useEffect(() => {
     // get user data based on url param. if data do not exist refer to 404 page.
@@ -35,7 +48,25 @@ const Profile = (props) => {
       .catch((err) => {
         console.log(err)
       })
-  }, [])
+  }, [props.match.params.userId])
+
+  const upload_data = {
+    name: "file",
+    action: "http://localhost:8080/api/users/upload-avatar/" + props.match.params.userId,
+    headers: {
+      authorization: "authorization-text",
+    },
+    onChange(info) {
+      if (info.file.status !== "uploading") {
+        console.log(info.file, info.fileList)
+      }
+      if (info.file.status === "done") {
+        message.success(`${info.file.name} file uploaded successfully`)
+      } else if (info.file.status === "error") {
+        message.error(`${info.file.name} file upload failed.`)
+      }
+    },
+  }
 
   return (
     <div className='container'>
@@ -45,6 +76,10 @@ const Profile = (props) => {
           <Avatar src={profileImg} size={140} className='avatar-pic'></Avatar>
           {userInformation.fname && userInformation.lname && <h3> {userInformation.fname + " " + userInformation.lname} </h3>}
           {userInformation.user_profession && <p> {userInformation.user_profession} </p>}
+
+          <Upload {...upload_data}>
+            <Button icon={<UploadOutlined />}>Upload</Button>
+          </Upload>
 
           <div className='row text-start'>
             <div className='col col-lg-3'>
