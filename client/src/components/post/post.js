4e4dbacfdd5d820591a1bb6c0427c3774e8adcaf
SoloNoah <39374814+SoloNoah@users.noreply.github.com>
2021-04-10 17:43:18 +0300

    responsiveness is done + states handling

--- client/src/components/post/post.js
+++ client/src/components/post/post.js
@@ -1,38 +1,63 @@
 import { Form, Input, Divider, Button } from 'antd';
-import { CameraOutlined, LinkOutlined, FileGifOutlined } from '@ant-design/icons';
-
+import { CameraOutlined, LinkOutlined, FileGifOutlined, CloseOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 import './post.css';
 import Tab from './posttabs/tab';
 import user from '../../assets/images/user.jpg';
 
 const { TextArea } = Input;
 const Post = () => {
+  const [active, setActive] = useState(false);
+  const [images, setImages] = useState();
+
+  const [linkState, setLinkState] = useState(false);
+  const [linkVal, setLinkVal] = useState('');
+
   const onChange = (e) => {
     console.log('Change:', e.target.value);
   };
+
+  const handleLinkChange = (e) => {
+    console.log(linkVal);
+    setLinkVal(e.target.value);
+  };
+
   return (
-    <div className='main-container'>
+    <Form className='main-container'>
+      <div className={active ? 'close-post cross visible' : ' close-post'}>
+        <CloseOutlined onClick={() => setActive(false)} />
+      </div>
       <div className='post-holder'>
         <div className='image-holder'>
           <img src={user} alt='user-img' />
         </div>
         <Form.Item className='input' name='post' rules={[{ required: true, message: 'Please add something before posting' }]}>
-          <TextArea maxLength={1000} onChange={onChange} size='large' placeholder='Share your knowledge...' />
+          <TextArea maxLength={1000} onChange={onChange} size='large' placeholder='Share your knowledge...' onClick={() => setActive(true)} />
         </Form.Item>
       </div>
-      <Divider />
-      <div className='tabs-holder'>
-        <Tab icon={<CameraOutlined />} name={'Photo'} />
-        <Tab icon={<LinkOutlined />} name={'Share link'} />
-        <Tab icon={<FileGifOutlined />} name={'Post GIF'} />
+      <Divider className='divider-active' />
+
+      <div className={images ? 'image-tab active' : 'image-tab'}>
+        <img src={images} alt='user image' />
       </div>
-      <Divider />
-      <div className='post-bottom'>
+
+      <div className={linkState ? 'link-tab active' : 'link-tab'}>
+        <Input className='input' size='large' placeholder='Enter the link URL' onChange={handleLinkChange} allowClear />
+        <CloseOutlined className='close-link cross' onClick={() => setLinkState(false)} />
+      </div>
+
+      <div className='tabs-holder'>
+        <Tab className='tab-btn' type='file' setImages={setImages} icon={<CameraOutlined />} name='Photo' />
+        <Tab className='tab-btn' icon={<LinkOutlined />} name='Share link' setLinkState={setLinkState} />
+        <Tab className='tab-btn' type='file' icon={<FileGifOutlined />} name='Post GIF' />
+      </div>
+      <Divider className={active ? 'active' : 'divider'} />
+      <div className={active ? 'post-bottom active' : 'post-bottom '}>
         <Button className='publish-post-btn' type='primary' block style={{ borderRadius: '5px' }}>
           {'Publish'}
         </Button>
       </div>
-    </div>
+    </Form>
   );
 };
 export default Post;
--- client/src/components/post/posttabs/tab.js
+++ client/src/components/post/posttabs/tab.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './tab.css';
+import { Upload, message, Button } from 'antd';
+
 const Tab = (props) => {
-  // need to center the label. doesn't center for some reason.
-  return (
-    <div className='tab'>
-      <div className='icon'>{props.icon}</div>
-      {props.name}
-    </div>
-  );
+  if (props.type === 'file') {
+    const config = {
+      // route action to upload router
+      // action="/upload",
+      beforeUpload: (file) => {
+        console.log(file);
+        return file.type === 'image/png' ? props.setImages(file.response.url) : message.error(`${file.name} is not a png file`);
+      },
+    };
+    return (
+      <Upload {...config}>
+        <Button className='tab' icon={props.icon}>
+          {props.name}
+        </Button>
+      </Upload>
+    );
+  } else {
+    const linkClick = () => {
+      props.setLinkState(true);
+    };
+    return (
+      <Button className='tab' icon={props.icon} onClick={linkClick}>
+        {props.name}
+      </Button>
+    );
+  }
 };
 
 export default Tab;
