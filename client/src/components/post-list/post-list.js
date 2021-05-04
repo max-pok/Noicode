cd7da7daeb5699465408d440bb54c0de77511d8f
Max Pokidaylo <maxim.p9@gmail.com>
2021-05-04 17:38:47 +0300

    add photo gallery [temporary], minor updates

--- client/src/components/post-list/post-list.js
+++ client/src/components/post-list/post-list.js
@@ -2,9 +2,98 @@ import "./post-list.css"
 import { Avatar, Image, Typography, Divider } from "antd"
 import { CommentOutlined, HeartOutlined, HeartFilled } from "@ant-design/icons"
 import React, { useState, useEffect } from "react"
+import Gallery from "react-photo-gallery"
 import axios from "axios"
 
-const { Text, Link } = Typography
+const { Text } = Typography
+
+export const photos = [
+  {
+    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    width: 1,
+    height: 1,
+  },
+  {
+    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    width: 3,
+    height: 4,
+  },
+  {
+    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    width: 3,
+    height: 4,
+  },
+  {
+    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    width: 3,
+    height: 4,
+  },
+  {
+    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    width: 3,
+    height: 4,
+  },
+  {
+    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/XiDA78wAZVw/600x799",
+    width: 3,
+    height: 4,
+  },
+  {
+    src: "https://source.unsplash.com/x8xJpClTvR0/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/u9cG4cuJ6bU/4927x1000",
+    width: 4927,
+    height: 1000,
+  },
+  {
+    src: "https://source.unsplash.com/qGQNmBE7mYw/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/NuO6iTBkHxE/800x599",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/pF1ug8ysTtY/600x400",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/A-fubu9QJxE/800x533",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://source.unsplash.com/5P91SF0zNsI/740x494",
+    width: 4,
+    height: 3,
+  },
+]
 
 const Posts = (props) => {
   const userPostUrl = "http://localhost:8080/api/posts/"
@@ -16,6 +105,29 @@ const Posts = (props) => {
       setPosts(response.data)
     })
   }, [])
+
+  const getPostsFiles = (fileIdArray) => {
+    let photos = []
+    fileIdArray.map((id, index) => {
+      if (id.length > 0) {
+        photos.push(
+          {
+            src: "http://localhost:8081/api/posts/" + id,
+            srcSet: "http://localhost:8081/api/posts/" + id,
+            width: 1,
+            height: 1,
+          },
+          {
+            src: "https://stackify.com/wp-content/uploads/2018/09/Java-Debugging-Tips-1280x720.jpg",
+            srcSet: "https://stackify.com/wp-content/uploads/2018/09/Java-Debugging-Tips-1280x720.jpg 500w",
+            width: 1,
+            height: 1,
+          }
+        )
+      }
+    })
+    return photos
+  }
 
   return posts.map((post, index) => {
     return (
@@ -38,11 +150,21 @@ const Posts = (props) => {
                 {/* <div className='col text-end'>save icon</div> */}
               </div>
               <br />
-              <div className='row'>{post.content}</div>
+              <br />
+              <div className='row post-content text-start'>{post.content}</div>
+              <div className='text-start'>
+                {post.tags.map((tag) => {
+                  return (
+                    <a key={tag} href={"https://google.com/" + tag}>
+                      #{tag}{" "}
+                    </a>
+                  )
+                })}
+              </div>
             </div>
           </div>
-
-          {post.img[0] && post.img[0].length > 0 && <Image src={"http://localhost:8081/api/posts/" + post._id} />}
+          {/* {post.img[0] && post.img[0].length > 0 && <Image src={"http://localhost:8081/api/posts/" + post.img[0]} />} */}
+          {post.img[0] && post.img[0].length > 0 ? <Gallery photos={getPostsFiles(post.img)} setComponentWidth={false} /> : <Divider className='post-divider' plain></Divider>}
 
           <div className='container'>
             <div className='row justify-content-end'>
