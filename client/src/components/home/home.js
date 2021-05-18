8e4b4a1acb5eb787203f12b066066697efff2686
Emilia <emiliazorin@gmail.com>
2021-05-18 11:12:24 +0300

    issue 34
    Signed-off-by: Emilia <emiliazorin@gmail.com>

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import Weather from "./weather/weather"
 import Post from '../post/post';
 import Posts from '../post-list/post-list';
@@ -11,21 +10,6 @@ const Home = (props) => {
         <div className="col">{/* <Weather /> */}</div>
         <div className="col-6">
           <Post />
-=======
-import Weather from "./weather/weather";
-import Post from "../post/post";
-import Posts from "../post-list/post-list";
-
-const Home = (props) => {
-  return (
-    <div className="container" style={{ paddingTop: "10px" }}>
-      <div className="row">
-        <div className="col">
-          <Weather />
-        </div>
-        <div className="col-6">
-          <Post path={"/users/:userId"} token={props.token} />
->>>>>>> 19164d418ffb6b7a6e96528e3cb371788d254435
           <br />
           <Posts posts={props.posts} clicked={props.clicked} setClicked={props.setClicked} />
         </div>
